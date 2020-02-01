a1bdf4f451604d339f0ce974bdd44addff717a21
mb0606 <marcoberardini@gmail.com>
2020-01-31 20:17:57 -0800

    custom useEffect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 function App() {
+
   const [notes, setNotes] = useState([]);
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
+
+  useEffect(() => {
+    // good for fetch data
+    const notesData = JSON.parse(localStorage.getItem('notes'));
+    if (notesData) setNotes(notesData);
+    console.log("will only run once")
+  }, [])
+
+  useEffect(() => {
+    console.log("will run every time I update my state")
+    localStorage.setItem("notes", JSON.stringify(notes));
+  }, [notes])
 
   const addNote = (e) => {
     e.preventDefault();
@@ -24,7 +37,7 @@ function App() {
         <Col></Col>
         <Col xs={6}><h1>Notes</h1>
           <Form onSubmit={addNote}>
-            {notes.map((note) => (
+            {notes && notes.map((note) => (
               <div key={note.title}>
                 <h4 onClick={() => removeNote(note.title)}>{note.title}</h4>
                 <p>{note.body}</p>
